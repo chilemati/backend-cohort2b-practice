123dceff1c8abfd5bd5a389aa63dfd66b8b9e0c7
chilemati <amadichile007@gmail.com>
2024-10-02 15:03:19 +0100

    v3

--- services/connectDb.js
+++ services/connectDb.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-let {DB_URL,DB_URL2} = process.env
+let {MONGODB_URI,DB_URL2} = process.env
 
 async function db (cb) {
     try {
-        result = await mongoose.connect(DB_URL);
+        result = await mongoose.connect(MONGODB_URI);
         // console.log(result.STATES.connected === 1)
         if(result.STATES.connected === 1) {
             console.log('db connected successfully!');
